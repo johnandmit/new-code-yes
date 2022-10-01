8732af1b303bb5a518ccd63054b567526937225f
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-10-01 17:47:57 +1300

    Update yasquen.c

--- yasquen.c
+++ yasquen.c
@@ -5,11 +5,11 @@ void placement(int n, int x, int y, int prev[n])
 {
     // check if x and y are valid
     for(int i = 0; i < y; i++){
-        if(prev[i] == -1) continue;
         if(x == prev[i]) return; // horizontal
         if(abs(x-prev[i]) == abs(y-i)) return; // diagonal
     }
 
+    prev[y] = x;
     // print when a solution is met
     if(y == n-1){
         printf("-----------\n");
@@ -18,7 +18,6 @@ void placement(int n, int x, int y, int prev[n])
         }
         printf("-----------\n");
     }else{
-        prev[y] = x;
         for(int i = 0; i < n; i++){
             placement(n, i, y+1, prev);
         }
@@ -30,10 +29,6 @@ int main(){
     printf("enter n: ");
     scanf("%i", &n);
     int prev[n];
-
-    for(int i = 0; i < n; i++){
-        prev[i] = -1;
-    }
 
     for(int i = 0; i < n; i++){
         placement(n, i, 0, prev);
