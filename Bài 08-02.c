7947c4ae8a6e061195ee736a55103b41d8605031
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-10-02 18:01:35 +1300

    Update Bài 08-02.c

--- Bài 08-02.c
+++ Bài 08-02.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 
-int recursion(int n, int count, int past, int present)
+int recursion(int n)
 {
-    if (count < n)
-        return recursion(n, ++count, present, past + present);
+    if (n <= 1)
+        return n;
+    else 
+        return recursion(n-1)+recursion(n-2);
+}
+
+int nonRecursion(int n)
+{
+    int present = 1, past = 0, temp;
+    for (int i = 1; i < n; i++)
+    {
+        temp = past;
+        past = present;
+        present = present + temp;
+    }
     return present;
 }
 
@@ -12,14 +25,6 @@ int main()
     int n;
     printf("enter n: ");
     scanf("%d", &n);
-    printf("%i\n\n", recursion(n-1, 1, 0, 1));
-
-    int present = 1, past = 0, temp;
-    for(int i = 1; i < n-1; i++)
-    {
-        temp = past;
-        past = present;
-        present = present + temp;
-    }
-    printf("%i", present);
+    printf("%i\n\n", recursion(n-1));
+    printf("%i", nonRecursion(n-1));
 }
