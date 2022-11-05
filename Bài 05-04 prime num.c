b99c509fec4dd10df8715a0fa079bc21fd16b99e
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-11-05 20:43:48 +1300

    Update Bài 05-04 prime num.c

--- Bài 05-04 prime num.c
+++ Bài 05-04 prime num.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <math.h>
+
 int main()
 {
-    int n, i = 2;
+    int n, i;
     printf("enter n: ");
     scanf("%i", &n);
     if (n < 2)
     {
-        printf("n is not prime");
+        printf("N is not prime");
     }
     else
     {
-        for (; i <= sqrt(n); i++)
+        for (i = 2; i <= sqrt(n); i++)
         {
             if (n % i == 0)
             {
-                printf("n is not prime");
+                printf("N is not prime");
                 return 0;
             }
         }
-        printf("n is prime");
+        printf("N is prime");
     }
 }
