44dfecf0f3402c6dea9b3b82ab7de9b5c731a9d5
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-11-05 20:56:51 +1300

    Update Bài 07-01.c

--- Bài 07-01.c
+++ Bài 07-01.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 void HanoiTower(int n, char start, char finish, char spare)
 {
     if(n == 1)
@@ -12,11 +13,14 @@ void HanoiTower(int n, char start, char finish, char spare)
         HanoiTower(n-1,spare,finish,start);
     }
 }
+
 int main()
 {
     int n;
     char start = 'a', spare = 'b', finish = 'c';
-    printf("enter num of disk: ");
+
+    printf("Enter num of disk: ");
     scanf("%i",&n);
+
     HanoiTower(n,start,finish,spare);
 }
