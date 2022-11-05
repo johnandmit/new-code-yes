abfa56977a866f0348ce19457072c8c057c6d152
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-11-05 19:45:11 +1300

    Update Bài 02-04.c

--- Bài 02-04.c
+++ Bài 02-04.c
@@ -4,18 +4,18 @@ int main()
 {
     int num ,i = -1 , arr[15];
     printf("enter: ");
-    scanf("%i",&num);
+    scanf("%i", &num);
 
     while(num != 0)
     {
-        arr[++i]= num % 2;
+        arr[++i] = num % 2;
         num /= 2;
     }
     
     printf("Binary: ");
     while(i >= 0)
     {
-        printf("%i",arr[i--]);
+        printf("%i", arr[i--]);
     }
     return 0;
 }
