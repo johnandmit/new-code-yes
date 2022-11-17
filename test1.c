bf72cccac9955c35d53a36aa9be7018ada9b4b81
johnandmit <95743485+johnandmit@users.noreply.github.com>
2022-11-17 16:56:26 +1300

    Update test1.c

--- test1.c
+++ test1.c
@@ -20,38 +20,41 @@ ddlNode *at(ddlNode *header, int place)
     return temp;
 }
 
-ddlNode *Insert(ddlNode *prev, int X, ddlNode *header)
+ddlNode *Insert(ddlNode *prev, int X)
 {
-    ddlNode *Temp;
-    Temp = (ddlNode *)malloc(sizeof(ddlNode));
-    Temp->info = X;
-    if (prev->next != NULL)
-    {
-        Temp->next = prev->next;
-    }
-    else
-    {
-        Temp->next = header;
-    }
-    Temp->prev = prev;
-    prev->next = Temp;
-    return Temp;
+    ddlNode *TempNode;
+    TempNode = (ddlNode *)malloc(sizeof(ddlNode));
+    TempNode->info = X;
+    TempNode->next = prev->next;
+    TempNode->prev = prev;
+    prev->next = TempNode;
+    return TempNode;
 }
 
 void remover(ddlNode *dlist, int key)
 {
     ddlNode *temp = dlist;
-    while (1)
+    while (temp != NULL)
     {
         if (temp->info == key)
         {
-            printf("%i ", temp->info);
             ddlNode *TempNode = temp->prev;
             TempNode->next = temp->next;
             temp->next->prev = TempNode;
         }
         temp = temp->next;
-        if (temp == dlist)
+    }
+}
+
+ddlNode *printer(ddlNode *header)
+{
+    ddlNode *TempNode;
+    TempNode = header->next;
+    while (1)
+    {
+        printf("%i ", TempNode->info);
+        TempNode = TempNode->next;
+        if (TempNode == NULL)
             break;
     }
 }
@@ -63,7 +66,7 @@ int main()
     int choice = 0, x, place;
     while (1)
     {
-        printf("1. insert\n2. remove\n3. end programme\nEnter: ");
+        printf("\n1. insert\n2. remove\n3. end programme\nEnter: ");
         scanf("%i", &choice);
         if (choice == 3)
         {
@@ -75,13 +78,16 @@ int main()
             scanf("%i", &place);
             printf("enter number you want to insert: ");
             scanf("%i", &x);
-            Insert(at(header, place), x, header);
+            Insert(at(header, place), x);
         }
         else if (choice == 2)
         {
             printf("enter key you want to remove: ");
             scanf("%i", &x);
+            printer(header);
+            printf("\n\n\n");
             remover(header, x);
+            printer(header);
         }
     }
 }
